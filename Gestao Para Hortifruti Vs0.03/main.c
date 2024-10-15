5ac73dc9392d46bf67dcb341ba276309f3998cc6
Rafael <rafaelgeres@gmail.com>
2024-10-14 23:54:39 -0300

    versão 0.3

---
+++ Gestao Para Hortifruti Vs0.03/main.c
@@ -0,0 +1,13 @@
+﻿/* Sistema que ajuda o hortifruti */
+#
+#include "Estrutura.h"
+
+
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	MenuDeGestaoHortifrut();
+	return 0;
+}
+
+
--- Gestão Hortifruti PIM/Gestão Hortifruti PIM/include/validation.h
+++
@@ -1,9 +0,0 @@
-#ifndef VALIDATION_H
-#define VALIDATION_H
-#include "Estruturas.h"
-
-#define MAX_PRODUCTS 100
-
-int ValidateProductFields(Products *mercadorias);
-
-#endif
