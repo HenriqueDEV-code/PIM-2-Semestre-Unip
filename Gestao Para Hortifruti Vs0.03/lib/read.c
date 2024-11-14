54312c4f3f5c5dbef769717c8beb7f9f7033c686
HenriqueDEV-code <pobsg.oficialbr@gmail.com>
2024-11-13 22:19:09 -0300

    teste de front lista cadastro

--- Gestao Para Hortifruti Vs0.03/lib/read.c
+++ Gestao Para Hortifruti Vs0.03/lib/read.c
@@ -156,9 +156,9 @@ void leituraTodosProdutos(char** campos, int num_campos) {
 
 void ListarProduto() {
     Sleep(500);
-    system("CLS");
-    borda(120,30);
-    borda(120,5);
+	system("CLS");
+	borda(120, 5);
+	borda(120, 7);
     Console(5, 9);
     printf("\033[32m   Lista de produtos cadastrados:\n\033[0m");
 
