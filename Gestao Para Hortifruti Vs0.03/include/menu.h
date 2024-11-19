99c7784e41609323f4a0328a2ace17686c5c8890
Rafael <rafaelgeres@gmail.com>
2024-11-19 14:21:17 -0300

    limpa nos arquivos visual studio

--- Gestao Para Hortifruti Vs0.03/include/menu.h
+++ Gestao Para Hortifruti Vs0.03/include/menu.h
@@ -6,13 +6,8 @@ void FluxoDeCaixa(void);
 void EntradaMercadoria(void);
 void RelatorioDeSistema(void);
 void QUEMSOMOS(void);
-<<<<<<< HEAD
 void MenuDeGestaoHortifrut(void);
-
-=======
-int MenuDeGestaoHortifrut(void);
 void FeedBack(void);
 void Doacoes(void);
->>>>>>> 949c53d08dbeff43b65325cdad3fdde9c411e012
 
 #endif
